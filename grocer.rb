f090adb37d3099dd9650e4ad51bb6e7c4d343e3d
Sara Khandaker <kttsara@hotmail.com>
2020-03-01 20:12:09 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -47,6 +47,7 @@ def apply_coupons(cart, coupons)
           newc[-1][:item]+= " W/COUPON"
           newc[-1][:price]=coupons[x][:cost]/ coupons[x][:num]
           newc[-1][:count]=coupons[x][:num]
+          binding.pry
           newc<< cart[i]
           newc[-1][:count]=cart[i][:count]-coupons[x][:num]
         else newc[-1][:count] == coupons[x][:num]
